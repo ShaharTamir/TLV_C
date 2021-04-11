dceb0321c54daca51d288e22d37a399eafd40eba
shahar_tamir <67794575+ShaharTamir@users.noreply.github.com>
2021-04-11 08:13:59 +0300

    Added drinks printers

--- drinksMachine.c
+++ drinksMachine.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdio.h> /* printf */
 
 enum drinkOp
 {
@@ -27,16 +27,33 @@ typedef struct
 
 }  drink;
 
+typedef void (*drinkPrinter)(void);
+
 static const int g_baseDrinks = 10;
-static drink g_drinksMachine[NUM_DRINKS_EXIT];
 static const unsigned int g_coinsFactor[NUM_COINS] = {1, 2, 5, 10};
 
+static int g_fontSize = 0;
+static drink g_drinksMachine[NUM_DRINKS_EXIT];
+static drinkPrinter g_drinksPrinter[NUM_DRINKS_EXIT];
+
 void init();
+void handleFontSize();
 int menu();
 unsigned int receivePayment();
 void handleChange(unsigned int change);
 void handlePayment(unsigned int price);
 void runMachine(int option);
+
+/* printers*/
+void printWater();
+void printOrange();
+void printApple();
+void printSprite();
+void printCola();
+
+void printPyramid(char *string);
+void printBackwardPyramid(char *string);
+
 
 int main()
 {
@@ -44,22 +61,30 @@ int main()
     int option = 0;
 
     init();
+    handleFontSize();
 
-    while(!finish)
+    if(g_fontSize)
     {
-        option = menu();
+        while(!finish)
+        {
+            option = menu();
 
-        if(option != NUM_DRINKS_EXIT)
-        {
-            runMachine(option);
+            if(option != NUM_DRINKS_EXIT)
+            {
+                runMachine(option);
+            }
+            else
+            {
+                finish = 1;
+            }
         }
-        else
-        {
-            finish = 1;
-        }
+
+        printf("Good bye.");
     }
-
-    printf("Good bye.");
+    else
+    {
+        printf("invalid size.");
+    }
 
     return 0;
 }
@@ -80,6 +105,19 @@ void init()
     g_drinksMachine[APPLE].name  = "apple juice";
     g_drinksMachine[SPRITE].name = "sprite";
     g_drinksMachine[COLA].name   = "cola";
+
+    g_drinksPrinter[WATER] = printWater;
+    g_drinksPrinter[ORANGE] = printOrange;
+    g_drinksPrinter[APPLE] = printApple;
+    g_drinksPrinter[SPRITE] = printSprite;
+    g_drinksPrinter[COLA] = printCola;
+
+}
+
+void handleFontSize()
+{
+    printf("Please enter drawing's size: ");
+    scanf("%d", &g_fontSize);
 }
 
 int menu()
@@ -120,6 +158,7 @@ void runMachine(int option)
     printf("The price is %d nis.\n", g_drinksMachine[option].price);
     
     handlePayment(g_drinksMachine[option].price);
+    g_drinksPrinter[option]();
 }
 
 void handlePayment(unsigned int price)
@@ -184,4 +223,120 @@ void handleChange(unsigned int change)
 
 	printf("You get back (1, 2, 5, 10): %d %d %d %d\n", changeInCoins[SHEKEL], 
                 changeInCoins[SHNEKEL], changeInCoins[FIVE_SHEKELS], changeInCoins[TEN_SHEKELS]);
+}
+
+void printWater()
+{
+    const int wReturns = 6;
+
+    for(int i = 0; i < g_fontSize * 2; ++i)
+    {
+        for(int j = 0; j < wReturns; ++j)
+        {
+            if((i == g_fontSize || i == g_fontSize - 1) && j && j < wReturns - 1)
+            {
+                printf(" ");
+            }
+            else
+            {
+                printf("W");
+            }
+        }
+
+        printf("\n");
+    }
+
+}
+
+void printOrange()
+{
+    for(int i = 0; i < g_fontSize; ++i)
+    {
+        for(int j = 0; j <= i; ++j)
+        {
+            printf("O");
+        }
+
+        printf("\n");
+    }
+}
+
+void printApple()
+{
+    for(int i = 0; i < g_fontSize; ++i)
+    {
+        for(int j = i + 1; j < g_fontSize; ++j)
+        {
+            printf(" ");
+        }
+        for(int k = 0; k <= i; ++k)
+        {
+            printf("A");
+        }
+
+        printf("\n");
+    }
+}
+
+void printSprite()
+{
+    printPyramid("S");
+}
+
+void printCola()
+{
+    printBackwardPyramid("C");
+    printPyramid("c");
+}
+
+void printPyramid(char *string)
+{
+    for(int i = 1; i <= g_fontSize; ++i)
+    {
+        int count = 1;
+
+        for(int j = i + 1; j <= g_fontSize; ++j)
+        {
+            printf(" ");
+        }
+
+        for(; count < i; ++count)
+        {
+            printf("%s", string);
+        }
+
+        while(count)
+        {
+            printf("%s", string);
+            --count;
+        }
+
+        printf("\n");
+    }
+}
+
+void printBackwardPyramid(char *string)
+{
+    for(int i = g_fontSize; i > 0; --i)
+    {
+        int count = 1;
+
+        for(int j = i + 1; j <= g_fontSize; ++j)
+        {
+            printf(" ");
+        }
+
+        for(; count < i; ++count)
+        {
+            printf("%s", string);
+        }
+
+        while(count)
+        {
+            printf("%s", string);
+            --count;
+        }
+
+        printf("\n");
+    }
 }
